9384338616312b149a36373687f7ee423a837123
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-05 21:11:34 +0300

    Ejsed home page.

--- index.mjs
+++ index.mjs
@@ -1,11 +1,29 @@
 import express from "express";
 import dotEnv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const Message = (title, text, author) => {
+    return { title, text, author, dt: new Date() };
+};
 
 const app = express();
 
-const port = dotEnv.config().parsed.PORT || 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+let messages = [];
+
+messages.push(Message("Hello", "Hello there", "Tommi"));
+messages.push(Message("Hi", "Hi there", "Mike"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index", { title: "Fancy Message Board", h1: "Messages", messages: messages });
 });
 
+const port = dotEnv.config().parsed.PORT || 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
