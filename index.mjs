ccce0aba8d31b8a0b93e005a5b0d22d4f5d2ea7c
Daniel X Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-05 21:26:27 +0300

    Finished (#1)
    * html and css
    
    * Ejsed home page.
    
    * Ejsed new
    
    * Add some comments

--- index.mjs
+++ index.mjs
@@ -1,11 +1,55 @@
 import express from "express";
 import dotEnv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
+// The model: Message
+const Message = (title, text, author) => {
+  return { title, text, author, dt: new Date() };
+};
+
+// The data: messages, and add some initial messages
+let messages = [];
+
+messages.push(Message("Hello", "Hello there", "Tommi"));
+messages.push(Message("Hi", "Hi there", "Mike"));
+
+// Set up the express app
 const app = express();
 
-const port = dotEnv.config().parsed.PORT || 8080;
+// Set up the view engine
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// Get routers for / and /new
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index", {
+    title: "Fancy Message Board",
+    h1: "Messages",
+    messages: messages,
+  });
 });
 
+app.get("/new", (req, res) => {
+  res.render("new", {
+    title: "New Message",
+    h1: "Add New Message",
+  });
+});
+
+// Middleware to parse the request body
+app.use(express.urlencoded({ extended: true }));
+
+// Post route for /new
+app.post("/new", (req, res) => {
+  messages.push(Message(req.body.title, req.body.text, req.body.author));
+  res.redirect("/");
+});
+
+// Start the server
+const port = dotEnv.config().parsed.PORT || 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
